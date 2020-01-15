840b3f3bb643337a711a08cf224cc45aabc2d28e
Interitio <cona@thewisewolf.dev>
2020-01-16 03:16:15 +1000

    Moved cmdClient to submodule

--- bot/main.py
+++ bot/main.py
@@ -2,13 +2,16 @@ import os
 
 from config import conf
 from logger import log
-from cmdClient import cmdClient
+from cmdClient.cmdClient import cmdClient
 
 # Get the real location
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
-client = cmdClient()
+masters = [int(master.strip()) for master in conf['masters'].split(",")]
+print(masters)
+
+client = cmdClient(prefix=conf['prefix'], owners=masters)
 client.load_dir(os.path.join(__location__, 'commands'))
 
 log("Initial setup complete, logging in", context='SETUP')
