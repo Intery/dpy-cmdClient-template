feda1de2cccf6bd1e0fe26e488cc3f4ac6a59d64
Interitio <cona@thewisewolf.dev>
2020-03-19 17:08:11 +1000

    feature (data module): Add `BotData` system for storing config data.

--- bot/main.py
+++ bot/main.py
@@ -4,15 +4,24 @@ from config import conf
 from logger import log
 from cmdClient.cmdClient import cmdClient
 
+from BotData import BotData
+
 # Get the real location
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
+# Load required data from configs
 masters = [int(master.strip()) for master in conf['masters'].split(",")]
-print(masters)
+data = BotData(app="main", data_file="data/config_data.db")
 
+# Initialise the client
 client = cmdClient(prefix=conf['prefix'], owners=masters)
+client.data = data
+client.log = log
+
+# Load the commands
 client.load_dir(os.path.join(__location__, 'commands'))
 
+# Log and execute!
 log("Initial setup complete, logging in", context='SETUP')
 client.run(conf['TOKEN'])
